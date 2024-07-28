f796c41837ea845b595e6a40da2d57480c28f29c
Olalekan <asaajulekan@gmail.com>
2024-07-28 13:32:44 +0100

    Model and table logic written

--- ecommerce/database/migrations/2024_07_28_061348_create_brands_table.php
+++ ecommerce/database/migrations/2024_07_28_061348_create_brands_table.php
@@ -11,12 +11,12 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('categories', function (Blueprint $table) {
+        Schema::create('brands', function (Blueprint $table) {
             $table->id();
             $table->string('name');
             $table->string('slug')->unique();
             $table->string('image')->nullable();
-            $table->boolean('is_active')->default(true);
+            $table->boolean('is_active')->default(true);  
             $table->timestamps();
         });
     }
@@ -26,6 +26,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('categories');
+        Schema::dropIfExists('brands');
     }
 };
