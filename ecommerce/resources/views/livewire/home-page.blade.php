7d8261edf07e395e76302940aeeb0e0700083545
Olalekan <asaajulekan@gmail.com>
2024-08-01 05:02:00 +0100

    tailwinds and livewire implemented

---
+++ ecommerce/resources/views/livewire/home-page.blade.php
@@ -0,0 +1,3 @@
+<div>
+    <h1 class="text-4x1 text-red-500">Hello livewire</h1>
+</div>
--- ecommerce/tailwind.config.js
+++ ecommerce/tailwind.config.js
@@ -1,9 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./resources/**/*.blade.php",
+    "./resources/**/*.js",
+    "./resources/**/*.vue",
+    'node_modules/preline/dist/*.js',
+  ],
+  darkMode:'class',
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [ require('preline/plugin'),
+
+  ],
 }
 
